028a04485290d9cf317c3f8ff9e8d1d2642f90ef
givenng6 <givenng6@gmail.com>
2022-08-25 15:57:33 +0200

    iOS working

--- android/app/src/main/kotlin/com/example/sdp_wits_services/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sdp_wits_services/MainActivity.kt
@@ -2,5 +2,4 @@ package com.example.sdp_wits_services
 
 import io.flutter.embedding.android.FlutterActivity
 
-class MainActivity: FlutterActivity() {
-}
+class MainActivity: FlutterActivity()
--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,19 @@
 import UIKit
 import Flutter
+//import FirebaseCore
+
 
 @UIApplicationMain
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate:FlutterAppDelegate {
+
+  //var window: UIWindow?
+
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
+    //FirebaseApp.configure()
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 }
