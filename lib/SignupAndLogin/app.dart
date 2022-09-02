7f72d7eafbc795ac9efb969d57fabf3690934d25
NuttyChuma <nuttychuma@gmail.com>
2022-09-02 18:19:19 +0200

    integration tests

--- lib/SignupAndLogin/app.dart
+++ lib/SignupAndLogin/app.dart
@@ -17,11 +17,8 @@ class _App extends State<App> {
         width: double.infinity,
         padding: const EdgeInsets.fromLTRB(0, 0, 0, 50),
         decoration: const BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/uni.jpeg'),
-            fit: BoxFit.cover
-          )
-        ),
+            image: DecorationImage(
+                image: AssetImage('assets/uni.jpeg'), fit: BoxFit.cover)),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.end,
           children: [
@@ -31,33 +28,53 @@ class _App extends State<App> {
                 height: 55,
                 margin: const EdgeInsets.symmetric(vertical: 5),
                 child: ElevatedButton(
+                    key: const Key('Continue as Student'),
                     style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff003b5c)),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                            )
-                        )
-                    ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xff003b5c)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ))),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => StudentsLoginScreen()));
-                    }, child: const Text("Continue as Student", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Color(0xff7393B3))))),
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => StudentsLoginScreen()));
+                    },
+                    child: const Text("Continue as Student",
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Color(0xff7393B3))))),
             Container(
                 width: 300,
                 height: 55,
                 margin: const EdgeInsets.symmetric(vertical: 5),
                 child: ElevatedButton(
+                    key: const Key('Continue as Staff'),
                     style: ButtonStyle(
-                        backgroundColor: MaterialStateProperty.all<Color>(const Color(0xff003b5c)),
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                            RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(50),
-                            )
-                        )
-                    ),
+                        backgroundColor: MaterialStateProperty.all<Color>(
+                            const Color(0xff003b5c)),
+                        shape:
+                            MaterialStateProperty.all<RoundedRectangleBorder>(
+                                RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                        ))),
                     onPressed: () {
-                      Navigator.push(context, MaterialPageRoute(builder: (context) => const StaffLoginScreen()));
-                    }, child: const Text("Continue as Staff", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16, color: Color(0xff7393B3)),)))
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const StaffLoginScreen()));
+                    },
+                    child: const Text(
+                      "Continue as Staff",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Color(0xff7393B3)),
+                    )))
           ],
         ),
       ),
