708e26d392765bf7d3651787feffccfae76cf9dc
givenng6 <givenng6@gmail.com>
2022-10-02 20:55:12 +0200

    ccdu form

--- lib/StudentsApp/CCDU/CCDU.dart
+++ lib/StudentsApp/CCDU/CCDU.dart
@@ -17,16 +17,79 @@ class CCDU extends HookWidget{
     var isSubscribed = useState(true);
     data = [email, title, service];
 
+    String meetingLocation = 'Online';
+    String theCounsellor = '';
+    List<String> places = ['Online', 'OnSite'];
+    List<String> counsellors = ['', 'Given', 'Mathebula'];
+
     return Scaffold(
+
+      
       appBar: AppBar(title: const Text('CCDU'), backgroundColor: Color(0xff003b5c),),
       floatingActionButton: FloatingActionButton.extended(
         onPressed: (){
           showModalBottomSheet(
               context: context,
               builder: (builder) => Container(
-                height: double.infinity,
+                //height: double.infinity,
                 padding: const EdgeInsets.all(12),
-                child: Text('New Session'),
+                child: Column(
+                 crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                  Text('New Session', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
+                  Text("Date"),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text("12/12/2010"),
+                        TextButton(onPressed: (){},
+                            child: Text('Change Date')
+                        ),
+                      ],
+                    ),
+                  Text("Time"),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text("12:00"),
+                      TextButton(onPressed: (){},
+                          child: Text('Change Time')
+                      ),
+                    ],
+                  ),
+                  Text('Meeting Location'),
+                  DropdownButton(
+                      isExpanded: true,
+                      value: meetingLocation,
+                      items: places.map((String places){
+                        return DropdownMenuItem(
+                          value: places,
+                          child: Text(places),
+                        );
+                      }).toList(),
+                      onChanged: (String? place){
+                        // must update value
+                        meetingLocation = place!;
+                      }),
+                  Text('Choose Counsellor (optional)'),
+                  DropdownButton(
+                      isExpanded: true,
+                      value: theCounsellor,
+                      items: counsellors.map((String counsellors){
+                        return DropdownMenuItem(
+                          value: counsellors,
+                          child: Text(counsellors),
+                        );
+                      }).toList(),
+                      onChanged: (String? counsellor){
+                        // must update value
+                        theCounsellor = counsellor!;
+                      }),
+                Text('Add Description'),
+                TextField(
+
+                  )
+                ],)
               ),
           );
         },
