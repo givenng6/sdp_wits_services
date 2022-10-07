c6bbebda6d05598d1a5806e35f37cc90cfd125b5
Khotsofalang <kgtsunyane@gmail.com>
2022-10-07 17:03:03 +0200

    just added ccdu staff page

--- lib/StaffApp/Dining/ccdu.dart
+++ lib/StaffApp/Dining/ccdu.dart
@@ -2,6 +2,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sdp_wits_services/StudentsApp/CCDU/CCDUObject.dart';
 
 class ccdu extends StatefulWidget {
   const ccdu({Key? key}) : super(key: key);
@@ -9,8 +10,12 @@ class ccdu extends StatefulWidget {
   @override
   State<ccdu> createState() => ccduState();
 }
+
+
 class ccduState extends State<ccdu>{
 
+  // TODO fil the bookings array with data from the database
+  List<dynamic> bookings = [{'name': 'Khotsos', 'email': 'example@email.com', 'date': '12/12/2020', 'time': '10:30', 'description': 'CCDU staff app'}];
 
   @override
   Widget build(BuildContext context) {
@@ -19,182 +24,77 @@ class ccduState extends State<ccdu>{
           colorSchemeSeed: const Color(0xff6750a4), useMaterial3: true),
       home: Scaffold(
         appBar: AppBar(title: const Text('CCDU'), backgroundColor: Color(0xff003b5c),),
-        body: Column(
-          children: const <Widget>[
-            //Spacer(),
-            Appoitment1(),
-            Appoitment2(),
-            Appoitment3(),
-            Appoitment4(),
-            //Spacer(),
+        body: bookings.isNotEmpty?
+        SingleChildScrollView(
+          child: Column(
+          children:[
+
+            makeList()
+          ],
+        )
+        )
+            : Center(
+          child: Text("Requests empty", style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20, color: Colors.grey),),
+        )
+      ),
+    );
+  }
+
+  Widget makeList(){
+    List<Widget> listCards = [];
+
+    for(var booking in bookings){
+      listCards.add(_card(booking['name'], booking['email'], booking['date'], booking['time'], booking['description']));
+    }
+
+    return Column(children: listCards);
+  }
+
+  Widget _card(String name, String email, String date, String time, String description){
+    return Card(
+      child: Container(
+        width: double.infinity,
+        padding: EdgeInsets.all(12),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text("Name: $name"),
+            Text("Email: $email"),
+            Text("Date: $date"),
+            Text("Time: $time"),
+            Text("Description"),
+            Text(description),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+
+              children: [
+                Container(
+                  margin: EdgeInsets.fromLTRB(0, 0, 12, 0),
+                  child:  ElevatedButton(onPressed: (){
+
+                    // TODO Bonga to implement logic
+
+                  }, child: Text('DECLINE')),
+                ),
+
+                ElevatedButton(onPressed: (){
+
+                  // TODO Bonga to implement logic
+                }, child: Text('ACCEPT')),
+
+              ],
+            )
           ],
         ),
+        // style: TextStyle(fontSize: 16),),
       ),
-    );
-  }
-}
 
-class Appoitment1 extends StatelessWidget {
-  const Appoitment1({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const Center(
-      child: Card(
-        child: SizedBox(
-          width: 350,
-          height: 170,
-          child:Text(''' Date: 06/10/2022
- Time: 09h:00
- Student name: Khotsofalang
- Description: Course is chowing me
- ''''',style: TextStyle(fontSize: 20),),
-          // style: TextStyle(fontSize: 16),),
-        ),
-      ),
-    );
-  }
-}
-
-class  Appoitment2 extends StatelessWidget {
-  const  Appoitment2({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        //elevation: 0,
-        //color: Theme.of(context).colorScheme.surfaceVariant,
-        child: const SizedBox(
-          width: 350,
-          height: 170,
-          child: Text(''' Date: 12/01/2022
- Time: 10h: 30
- Student name: khotsos
- Descprption: its tough
- ''''',style: TextStyle(fontSize: 20),),
-          // style: TextStyle(fontSize: 16),),
-        ),
-      ),
-    );
-  }
-}
-
-class  Appoitment3 extends StatelessWidget {
-  const  Appoitment3({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        //elevation: 0,
-        //color: Theme.of(context).colorScheme.surfaceVariant,
-        child: const SizedBox(
-          width: 350,
-          height: 170,
-          child: Text(''' Date: 08/10/2022
- Time: 11h:00
- Student name: Tsunyane
- Descprption: mxm
- ''''',style: TextStyle(fontSize: 20),),
-          // style: TextStyle(fontSize: 16),),
-        ),
-      ),
-    );
-  }
-}
-
-class  Appoitment4 extends StatelessWidget {
-  const  Appoitment4({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-
-
-    return Center(
-      child: Card(
-        //elevation: 0,
-        //color: Theme.of(context).colorScheme.surfaceVariant,
-        child: const SizedBox(
-          width: 350,
-          height: 170,
-          child: Text(''' Date: 07/10/2022
- Time: 13h:00
- Student name: khotso
- Descprption: bla bla bla bla
-  ''''',style: TextStyle(fontSize: 20),),
-          // style: TextStyle(fontSize: 16),),
-        ),
-
-      ),
     );
   }
 }
 
 
-//     return Scaffold(
-//         appBar: AppBar(title: const Text('CCDU'), backgroundColor: Color(0xff003b5c),),
-//          body : Card(
-//       clipBehavior: Clip.antiAlias,
-//       shape: RoundedRectangleBorder(
-//         borderRadius: BorderRadius.circular(24),
-//       ),
-//       child: Column(
-//         children: [
-//           Stack(
-//             children: [
-//               Ink.image(
-//                 image: NetworkImage(
-//                   'https://www.iconsdb.com/icons/preview/white/rounded-rectangle-xxl.png',
-//                 ),
-//                 height: 240,
-//                 fit: BoxFit.cover,
-//               ),
-//               Positioned(
-//                 bottom: 16,
-//                 right: 16,
-//                 left: 16,
-//                 child: Text(
-//                   'Appoitment1',
-//                   style: TextStyle(
-//                     fontWeight: FontWeight.bold,
-//                     color: Colors.blue,
-//                     fontSize: 24,
-//                   ),
-//                 ),
-//               ),
-//             ],
-//           ),
-//           Padding(
-//             padding: EdgeInsets.all(16).copyWith(bottom: 0),
-//             child: Text(
-//               ''''date:
-//                   time:
-//                   student name:
-//                   descprption:
-//                 ''''',style: TextStyle(fontSize: 16),
-//               //style: TextStyle(fontSize: 16),
-//             ),
-//           ),
-//           ButtonBar(
-//             alignment: MainAxisAlignment.start,
-//             children: [
-//               FlatButton(
-//                 child: Text('Accept'),
-//                 onPressed: () {},
-//               ),
-//               FlatButton(
-//                 child: Text('Reject'),
-//                 onPressed: () {},
-//               )
-//             ],
-//           )
-//         ],
-//       ),
-//     )
-//     );
-//
-//   }
-// }
+
 
 
