6ccb179a59e0455949edf3661cd37b778bc5e957
NuttyChuma <nuttychuma@gmail.com>
2022-10-12 11:53:35 +0200

    removed ukupanyaza

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,8 +1,13 @@
 library globals;
 
 import 'package:flutter/cupertino.dart';
-import 'package:flutter_hooks/flutter_hooks.dart';
+import 'package:sdp_wits_services/SignupAndLogin/app.dart';
+import 'package:sdp_wits_services/StaffApp/Buses/buses_main.dart';
+import 'package:sdp_wits_services/StaffApp/Dining/mealSelectionPage.dart';
+import 'package:sdp_wits_services/StaffApp/StaffPage.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+
+import 'StudentsApp/Home/Start.dart';
 
 String? username;
 String? email;
@@ -16,8 +21,22 @@ Future getSharedPreferences() async{
   kind = sharedPreferences.getString('kind');
   department = sharedPreferences.getString('department');
   debugPrint('called');
-  // debugPrint(email);
-  // debugPrint(username);
-  // debugPrint(kind);
-  // debugPrint(department);
+}
+
+Future getData() async {
+  if (username != "" && username != null && kind == 'Student') {
+    return Start(email: email!, username: username!);
+  }
+  else if (username != "" && username != null && kind == 'Staff' && department == 'Bus Services') {
+    return const BusesMain();
+  }
+  else if (username != "" && username != null && kind == 'Staff' && department == 'Dining Services') {
+    return const mealSelecionPage();
+  }
+  else if (username != "" && username != null && kind == 'Staff') {
+    return const StaffPage();
+  }
+  else{
+    return const App();
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'dart:ui';
 import 'package:animated_splash_screen/animated_splash_screen.dart';
 import 'package:flutter/material.dart';
-import 'SignupAndLogin/app.dart';
 import 'firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'globals.dart' as globals;
@@ -9,11 +8,15 @@ import './StudentsApp/Providers/Subscriptions.dart';
 import './StudentsApp/Providers/UserData.dart';
 import 'package:provider/provider.dart';
 
+Widget? nextScreen;
 void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await globals.getSharedPreferences();
+  nextScreen = await globals.getData();
   runApp(
       MultiProvider(providers: [
         ChangeNotifierProvider(create: (_) => Subscriptions()),
@@ -52,7 +55,7 @@ class _MainState extends State<Main> {
         backgroundColor: const Color(0xff003b5c),
         function: getTo,
         splash: Image.asset("assets/white_logo_nb.png"),
-        nextScreen: const App(),
+        nextScreen: nextScreen!,
       ),
     );
   }
