423fe765bdaac672d890115ea9912212987bd03d
Khotsofalang <kgtsunyane@gmail.com>
2022-10-19 23:19:37 +0200

    just made some changes on ccdu

--- lib/StaffApp/CCDU/ccduGlobals.dart
+++ lib/StaffApp/CCDU/ccduGlobals.dart
@@ -2,20 +2,39 @@ library globals;
 
 import 'Booking.dart';
 
-List<Booking> AllBookings = [];
+List<Booking> AllBookings = [
+  Booking(type: "all", obj: {
+    "creator": "",
+    "time": "",
+    "date": "",
+    "counsellor": "",
+    "counsellorName": "",
+    "description": "bhiy",
+    "location": "Online"
+  }),
+  Booking(type: "all", obj: {
+    "creator": "",
+    "time": "",
+    "date": "",
+    "counsellor": "",
+    "counsellorName": "",
+    "description": "vyih",
+    "location": "Online"
+  }),
+  Booking(type: "all", obj: {
+    "creator": "",
+    "time": "",
+    "date": "",
+    "counsellor": "",
+    "counsellorName": "",
+    "description": "o",
+    "location": "inPerson"
+  }),
+];
 List<Booking> AcceptedBookings = [];
 
-Future<void> GetAllBookings()async{
+Future<void> GetAllBookings() async {}
 
+Future<void> GetAcceptedBookings() async {}
 
-
-}
-
-Future<void> GetAcceptedBookings()async{
-
-
-}
-
-Future<void> HnadleBooking(Booking booking)async{
-
-}
+Future<void> HnadleBooking(Booking booking) async {}
