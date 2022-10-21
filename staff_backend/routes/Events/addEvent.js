f6d178c9ea33be150a3f6176d2a8f5230537cf8e
NuttyChuma <nuttychuma@gmail.com>
2022-10-21 18:48:18 +0200

    add event

--- staff_backend/routes/Events/addEvent.js
+++ staff_backend/routes/Events/addEvent.js
@@ -1,12 +1,15 @@
 import express from "express";
 import { db } from "../../firebase-config.js";
-import { ref, child, get, update } from "firebase/database";
+import { doc, setDoc } from "firebase/firestore"; 
 
 const router = express.Router();
 
 router.post("/", async (req, res) => {
   const {event} = req.body;
-  await update(ref(db, `posts/${event.id}`), event);
+
+  await setDoc(doc(db, "Events", `${event.id}`), event);
+
+//   await update(ref(db, `Events/${event.id}`), event);
   res.send('done');
   console.log("updated");
 });
