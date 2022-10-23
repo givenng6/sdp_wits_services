9d1e73fd0181e23aaac8a06b89a84f47d949f250
NuttyChuma <nuttychuma@gmail.com>
2022-10-23 12:49:57 +0200

    cancel ride

--- lib/StudentsApp/Protection/book_ride.dart
+++ lib/StudentsApp/Protection/book_ride.dart
@@ -1,8 +1,10 @@
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:http/http.dart' as http;
 import 'package:provider/provider.dart';
+import 'package:sdp_wits_services/StudentsApp/Protection/protection.dart';
 import 'package:sdp_wits_services/StudentsApp/Providers/Subscriptions.dart';
 
 import '../Providers/UserData.dart';
@@ -26,6 +28,8 @@ class _BookRideState extends State<BookRide> {
 
   String email = "";
   String username = "";
+
+  final bookedController = Get.find<Booked>();
 
   @override
   Widget build(BuildContext context) {
@@ -152,6 +156,7 @@ class _BookRideState extends State<BookRide> {
         })
     ).then((value){
       context.read<Subscriptions>().setBooked(true);
+      bookedController.booked(true);
       Navigator.pop(context);
     });
   }
